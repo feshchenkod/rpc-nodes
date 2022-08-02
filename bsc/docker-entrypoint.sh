0fe9c92a5b538a150773558e16cbedef5f08cee3
_den <_den@outlook.com>
2022-08-02 17:50:08 +1000

    fixed paths after update to v1.1.12

--- bsc/docker-entrypoint.sh
+++ bsc/docker-entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 set -x
 
-if [ ! -f /root/.ethereum/setupdone ]; then
-    wget -O /root/mainnet.zip "https://github.com/bnb-chain/bsc/releases/download/${BSC_TAG}/mainnet.zip"
-    unzip -o -j /root/mainnet.zip "mainnet/genesis.json" -d /root/.ethereum/
-    bsc init /root/.ethereum/genesis.json
-    touch /root/.ethereum/setupdone
+if [ ! -f /bsc/config/setupdone ]; then
+    wget -O /bsc/mainnet.zip "https://github.com/bnb-chain/bsc/releases/download/${BSC_TAG}/mainnet.zip"
+    unzip -o -j /bsc/mainnet.zip "mainnet/genesis.json" -d /bsc/config/
+    bsc --datadir /bsc/.ethereum init /bsc/config/genesis.json
+    touch /bsc/config/setupdone
 fi
 
 exec "$@"
