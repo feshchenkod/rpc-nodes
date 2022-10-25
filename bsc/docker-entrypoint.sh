fca898933723394730b57a742deaf8395ec51502
_den <_den@outlook.com>
2022-10-25 19:11:09 +1000

    fixed bsc entrypoint

--- bsc/docker-entrypoint.sh
+++ bsc/docker-entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 set -x
 
-if [ ! -f /bsc/config/setupdone ]; then
+if [ ! -f /bsc/.ethereum/config/setupdone ]; then
     wget -O /bsc/mainnet.zip "https://github.com/bnb-chain/bsc/releases/download/${BSC_TAG}/mainnet.zip"
-    unzip -o -j /bsc/mainnet.zip "mainnet/genesis.json" -d /bsc/config/
-    bsc --datadir /bsc/.ethereum init /bsc/config/genesis.json
-    touch /bsc/config/setupdone
+    unzip -o -j /bsc/mainnet.zip "genesis.json" -d /bsc/.ethereum/config/
+    bsc --datadir /bsc/.ethereum init /bsc/.ethereum/config/genesis.json
+    touch /bsc/.ethereum/config/setupdone
 fi
 
 exec "$@"
